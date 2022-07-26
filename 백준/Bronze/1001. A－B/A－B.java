3a2fe801490d9ffc6466c7c4915e61b62296222d
jeeyunryu <68150327+jeeyunryu@users.noreply.github.com>
2022-07-26 20:10:33 +0900

    [Bronze V] Title: A-B, Time: 228 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner key = new Scanner(System.in);
+		int a = 0, b = 0;
+		int sum = 0;
+
+		a = key.nextInt();
+		b = key.nextInt();
+
+		sum = a - b;
+
+		System.out.println(sum);
+
+		key.close();
+
+	}
+}
